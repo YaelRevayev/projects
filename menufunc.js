471c66db3143530ed9da3c388de635641dc283e5
YaelRevayev <gexploreapp@gmail.com>
2022-11-18 12:53:14 +0200

    new changes

--- menufunc.js
+++ menufunc.js
@@ -119,3 +119,29 @@ function cellClicked(id){
 function find_card_by_id(card_id){
    return dyn_matrix[card_id[0]][card_id[1]]
 }
+
+
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links a");
+  
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("nav-active");
+  
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s `;
+        }
+      });
+      burger.classList.toggle("toggle");
+    });
+    //
+  };
+  
+  navSlide();
+  
